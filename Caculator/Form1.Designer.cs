1ad4140afc2f45f04e8e88a1b1b1b9510e94ae05
Vn-Yn <yvanha24@gmail.com>
2023-11-03 10:14:11 +0700

    Them xu ly tru

--- Caculator/Form1.Designer.cs
+++ Caculator/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -71,25 +72,35 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(215, 197);
+            this.button1.Location = new System.Drawing.Point(215, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 38);
             this.button1.TabIndex = 4;
             this.button1.Text = "Cong";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tru";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(687, 428);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -104,6 +115,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
